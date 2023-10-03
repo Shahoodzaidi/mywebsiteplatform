f70cc069bd29328601787eeb6262fcbb9b0e3983
shahood zaidi <144329859+Shahoodzaidi@users.noreply.github.com>
2023-10-03 18:56:44 +0530

    THIS IS MY SECOND COMMIT..

---
+++ web.js
@@ -0,0 +1,43 @@
+gsap.from(".nav",{
+    y:-130,
+    duration: 1,
+    opacity: 0,
+    
+})
+gsap.from("h3",{
+    y:-40,
+    duration: 1.1,
+   
+    delay:0.7,
+    
+})
+
+gsap.from("h1,span",{
+    y:200,
+    delay:0.5,
+    duration:2,
+    opacity:0,
+    
+})
+gsap.from("p",{
+    x:-530,
+    delay:1,
+    duration:2,
+    opacity:0,
+    
+})
+gsap.from("button",{
+    x:530,
+    delay:1.4,
+    duration:2,
+    opacity:0,
+    
+})
+gsap.from(".web2",{
+    scale:-2,
+    delay:1.5,
+    duration:1,
+    opacity:0,
+    
+    
+})
